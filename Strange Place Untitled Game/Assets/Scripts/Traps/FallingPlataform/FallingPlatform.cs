a8baf2a8dafdf6a2d89fdb5d31c2f02ed01521d4
ArthurPalladino <arthur.palladino1@gmail.com>
2026-02-17 16:10:38 -0300

    feat: add falling platform

---
+++ Strange Place Untitled Game/Assets/Scripts/Traps/FallingPlataform/FallingPlatform.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour, ITrap
+{
+    [SerializeField] float fallDelay;
+    [SerializeField] float destroyDelay;
+    Animator animator;
+    Rigidbody2D rb;
+
+    BoxCollider2D boxCollider;
+    public bool IsOn { get; set; }
+
+    private void Awake() 
+    {
+        animator = GetComponent<Animator>();    
+        boxCollider = GetComponent<BoxCollider2D>();    
+        rb = GetComponent<Rigidbody2D>();   
+    }
+    public void SetState(bool state)
+    {   
+        IsOn = state;
+        animator.enabled = IsOn;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        SetState(false);
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        boxCollider.enabled = false;
+        Destroy(gameObject,destroyDelay);
+
+    }
+}
