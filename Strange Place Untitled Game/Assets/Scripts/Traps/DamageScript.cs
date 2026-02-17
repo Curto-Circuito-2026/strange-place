29f98b9fd1804c05c7698496936d96d13f8794cc
Cadu Passaes <kadugrupassaes@gmail.com>
2026-02-17 16:21:09 -0300

    Código base do Espinho

--- Strange Place Untitled Game/Assets/Scripts/Traps/DamageScript.cs
+++ Strange Place Untitled Game/Assets/Scripts/Traps/DamageScript.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 
 public class DamageScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    
 
+    public int damage = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // puxa o script de dano :)
+        }
+    }
+    
+    
     // Update is called once per frame
     void Update()
     {
