b0aa92a9beff846f97735cb185dd8b226bb7bebc
Marlon Correia Bomfim Junior <42979884+marlaoza@users.noreply.github.com>
2026-02-23 14:10:01 -0300

    fix: wall jump + sounds + customer sprites

--- Strange Place Untitled Game/Assets/Scripts/Player/Movement/Jumps/NormalJump.cs
+++ Strange Place Untitled Game/Assets/Scripts/Player/Movement/Jumps/NormalJump.cs
@@ -16,6 +16,7 @@ public class NormalJump : MonoBehaviour, IJump
 
     public void Jump(Rigidbody2D rb)
     {
+
         Animator.SetTrigger("Jump");
         rb.linearVelocity = new Vector2(wallReflectForce*opositeForce, jumpForce);
         CanJump = false;
@@ -28,17 +29,19 @@ public class NormalJump : MonoBehaviour, IJump
 
         Vector2 normal = collision.contacts[0].normal;
 
+
         if (1f - Mathf.Abs(normal.x) <= 0.1f ) //wall
         {
             Animator.SetBool("inWall", true);
             CanJump = true;
-            opositeForce = transform.position.x - collision.transform.position.x < 0 ? 1 : -1;
+            opositeForce = normal.x;
 
         }
 
         else if (1f - Mathf.Abs(normal.y) <= 0.1f) // ground
         {
             Animator.SetBool("isGrounded", true);
+            Animator.SetBool("inWall", false);
             CanJump = true;
             opositeForce = 0;
         }
@@ -47,5 +50,6 @@ public class NormalJump : MonoBehaviour, IJump
     void OnCollisionExit2D(Collision2D collision)
     {
         Animator.SetBool("inWall", false);
+        
     }
 }
